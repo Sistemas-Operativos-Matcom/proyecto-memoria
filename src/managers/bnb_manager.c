6237b6796d19c002db2192939d15b1d678539958
RodryJR <dariordguezll@gmail.com>
2023-11-03 18:23:50 -0400

    bug

--- src/managers/bnb_manager.c
+++ src/managers/bnb_manager.c
@@ -2,57 +2,60 @@
 
 #include "stdio.h"
 
+
 // Esta función se llama cuando se inicializa un caso de prueba
-void m_bnb_init(int argc, char **argv) {
-  fprintf(stderr, "Not Implemented\n");
-  exit(1);
+void m_bnb_init(int argc, char **argv) 
+{
+  
 }
 
 // Reserva un espacio en el heap de tamaño 'size' y establece un puntero al
 // inicio del espacio reservado.
-int m_bnb_malloc(size_t size, ptr_t *out) {
-  fprintf(stderr, "Not Implemented\n");
-  exit(1);
+int m_bnb_malloc(size_t size, ptr_t *out) 
+{
+  return 1;
 }
 
 // Libera un espacio de memoria dado un puntero.
-int m_bnb_free(ptr_t ptr) {
-  fprintf(stderr, "Not Implemented\n");
-  exit(1);
+int m_bnb_free(ptr_t ptr) 
+{
+  return 1;
 }
 
 // Agrega un elemento al stack
-int m_bnb_push(byte val, ptr_t *out) {
-  fprintf(stderr, "Not Implemented\n");
-  exit(1);
+int m_bnb_push(byte val, ptr_t *out) 
+{
+  return 1;
 }
 
 // Quita un elemento del stack
-int m_bnb_pop(byte *out) {
-  fprintf(stderr, "Not Implemented\n");
-  exit(1);
+int m_bnb_pop(byte *out) 
+{
+  return 1;
 }
 
 // Carga el valor en una dirección determinada
-int m_bnb_load(addr_t addr, byte *out) {
-  fprintf(stderr, "Not Implemented\n");
-  exit(1);
+int m_bnb_load(addr_t addr, byte *out) 
+{
+  return 1;
 }
 
 // Almacena un valor en una dirección determinada
-int m_bnb_store(addr_t addr, byte val) {
-  fprintf(stderr, "Not Implemented\n");
-  exit(1);
+int m_bnb_store(addr_t addr, byte val) 
+{
+  return 1;
 }
 
 // Notifica un cambio de contexto al proceso 'next_pid'
-void m_bnb_on_ctx_switch(process_t process) {
+void m_bnb_on_ctx_switch(process_t process) 
+{
   fprintf(stderr, "Not Implemented\n");
   exit(1);
 }
 
 // Notifica que un proceso ya terminó su ejecución
-void m_bnb_on_end_process(process_t process) {
+void m_bnb_on_end_process(process_t process) 
+{
   fprintf(stderr, "Not Implemented\n");
   exit(1);
 }
