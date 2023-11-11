bfc9721313897d0e9ed19e6835c7faf8d5416e6e
Adrián Hernández <hadrianc02@gmail.com>
2023-11-11 17:06:06 -0500

    Corregidas las direcciones de bash del sistema
    *Por alguna razón, en mi máquina virtual Ubuntu el bash se encuentra en /usr/bin/bash, así que la cambié al principio y ahora estoy cambiándola a la que estaba por defecto

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 mkdir -p build
 gcc -o build/main $(find src | grep -P "\.c") -Wall -Wextra -Wno-unused-variable -Wno-unused-parameter
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 ./build.sh || exit 1
 
