3d6f2171b2e008248c216649973f65454377ea99
Osvaldo R.Moreno P <osvaldo020213@gmail.com>
2023-11-12 06:36:02 +0100

    algunos errores arreglados de pag

--- src/managers/pag_manager.c
+++ src/managers/pag_manager.c
@@ -6,13 +6,23 @@
 
 #ifndef VAL_CODE
 #define VAL_CODE printf("Val_46")
-#define fori(base, bound) for (size_t i = (size_t)base; i < (size_t)base + bound; i++)
+#define fori(base, bound) for (size_t i = (size_t)base; i < (size_t)(base + bound); i++)
 #define min(a, b) (a < b) ? a : b
 #define max(a, b) (a > b) ? a : b
 #define clamp(a, b, x) (x > b) ? b : (x < a) ? a \
                                              : x
 #define MAX_PROC_COUNT 20
 #endif
+
+const int MASTER = 1;
+int cero_messanges = 0; // 0 no allert
+
+void set_messanges(int on)
+{
+  if (MASTER)
+    return;
+  cero_messanges = on;
+}
 
 // Context
 int pag_cur_ipid;
@@ -26,15 +36,14 @@ int *pag_virtual_memory; // para el bug de free en el medio 0->Not USED  1-> USE
 
 // Physical Memory
 int *pag_free_page; // free list, ith pag is : 1->free 0->alloc
-int pag_pages_count;
+int pag_pages_count = 0;
 
 // Virtual Memory (Espacio de direcciones virtuales)
-int **pag_pages_table; // ith Procs pages table
-int **pag_pages_valid; // ith 1->valid 0->not valid
+int **pag_pages_table; // ith Procs pages table if page < 0 not valid
 
 // VPN and PA [INPUTS]
-size_t pag_page_size;
-size_t pag_offset_bits = 4; // 2^offset = page size
+size_t pag_page_size = 0;
+size_t pag_offset_bits = 7; // 2^offset = page size
 
 // FUNCTIONS
 // PIDS
@@ -55,12 +64,12 @@ addr_t pag_get_base_page(int physical_page_number)
   return physical_page_number * pag_page_size;
 }
 
-int pag_get_offset(addr_t addr)
+addr_t pag_get_offset(addr_t addr)
 {
   return addr & ((1 << pag_offset_bits) - 1);
 }
 
-int pag_get_vpn(addr_t addr)
+addr_t pag_get_vpn(addr_t addr)
 {
   return addr >> pag_offset_bits;
 }
@@ -68,21 +77,35 @@ int pag_get_vpn(addr_t addr)
 addr_t pag_get_pa(addr_t addr)
 {
   int pp = pag_pages_table[pag_cur_ipid][pag_get_vpn(addr)];
+  // if(pp<0)
+  // {
+  //   printf("va: %ld vpn: %ld\n", addr, pag_get_vpn(addr));
+  //   printf("page size: %ld   Offset bits : %ld  Offset: %ld\n", pag_page_size, pag_offset_bits, pag_get_offset(addr));
+  //   exit(0);
+  // }
   return pag_get_base_page(pp) + pag_get_offset(addr);
 }
 
 // PHYSICAL MEMORY
 
+int pag_is_page_valid(int page_number)
+{
+  return pag_pages_table[pag_cur_ipid][page_number] >= 0;
+}
+
 void pag_update_heap()
 {
-  fori(0, pag_get_vpn(pag_stack[pag_cur_ipid])) if (pag_pages_valid[pag_cur_ipid][i]) pag_heap[pag_cur_ipid] = i;
+  pag_heap[pag_cur_ipid] = -1;
+  fori(0, pag_get_vpn(pag_stack[pag_cur_ipid])) if (pag_is_page_valid(i)) pag_heap[pag_cur_ipid] = i;
+  pag_heap[pag_cur_ipid]++;
 }
 
 void pag_updated_valid_pages()
 {
+  pag_update_heap();
   for (int vpn = 0; vpn < pag_heap[pag_cur_ipid] + 1; vpn++)
   {
-    if (!pag_pages_valid[pag_cur_ipid][vpn])
+    if (!pag_is_page_valid(vpn))
       continue;
 
     int flag = 1;
@@ -95,36 +118,48 @@ void pag_updated_valid_pages()
         break;
       }
     }
+    // printf("page: %d vpn: %d flag %d heap: %d ", pp, vpn, flag, pag_heap[pag_cur_ipid]);
+    // exit(0);
     if (flag)
     {
       pag_free_page[pp] = 1;
-      pag_pages_valid[pag_cur_ipid][vpn] = 0;
+      pag_pages_table[pag_cur_ipid][vpn] = -1;
     }
   }
-  pag_update_heap();
 }
 
-int pag_alloc_free_memory(addr_t paddress, int while_x, int alloc)
+int pag_alloc_free_memory(addr_t va, int while_x, int alloc)
 {
-  printf("Alloc...:\n");
-  fori(paddress, while_x)
+  fori(va, while_x)
   {
-    if ((alloc && pag_virtual_memory[i]) || (!alloc && !pag_virtual_memory[i]))
+    // printf("addr: %ld ", pag_get_pa(i));
+
+    if (cero_messanges && ((alloc && pag_virtual_memory[pag_get_pa(i)]) || (!alloc && !pag_virtual_memory[pag_get_pa(i)])))
     {
-      printf("ERROR in memory manager Alloc?: %d,  is memory used?: %d\n", alloc, pag_virtual_memory[i]);
+      int a = pag_virtual_memory[pag_get_pa(i)];
+      printf("ERROR in memory manager Alloc: %d, Virtual Memo:%d,  Addr: 0x%zx \n", alloc, a, pag_get_pa(i));
       return 1;
     }
-    printf("0x%zx ", i);
-    pag_virtual_memory[i] = alloc;
+
+    pag_virtual_memory[pag_get_pa(i)] = alloc;
+
+    if (pag_get_vpn(i) != pag_get_vpn(va) && pag_get_pa(i) + 1 != pag_get_pa(i + 1)) // cambio de pagina
+    {
+      // printf("testing: %ld %ld", pag_get_pa(i), pag_get_pa(i + 1));
+      if (alloc)
+        m_set_owner(pag_get_pa(va), pag_get_pa(i - 1));
+      else
+        m_unset_owner(pag_get_pa(va), pag_get_pa(i - 1));
+      va = i;
+    }
   }
-  printf("\n");
+
   if (alloc)
-    m_set_owner(paddress, paddress + while_x - 1);
-
+    m_set_owner(pag_get_pa(va), pag_get_pa(va + while_x - 1));
   else
-    m_unset_owner(paddress, paddress + while_x - 1);
+    m_unset_owner(pag_get_pa(va), pag_get_pa(va + while_x - 1));
 
-  // pag_updated_valid_pages();
+  pag_updated_valid_pages();
   return 0;
 }
 
@@ -144,14 +179,14 @@ int pag_find_free_page()
 int _pag_find_free_continous_space(size_t size, addr_t *li)
 {
   int free_pages_base = pag_free_page_counting();
-  printf("free_pages_base: %d\n", free_pages_base);
+
   size_t free = 0;
   *li = __INT_MAX__;
   int free_pages = free_pages_base;
 
-  for (int vpn = 0; vpn < pag_pages_count; vpn++)
+  for (size_t vpn = 0; vpn < pag_get_vpn(pag_stack[pag_cur_ipid]); vpn++)
   {
-    if (pag_pages_valid[pag_cur_ipid][vpn])
+    if (pag_is_page_valid(vpn))
     {
       int pp = pag_pages_table[pag_cur_ipid][vpn];
 
@@ -186,35 +221,30 @@ int _pag_find_free_continous_space(size_t size, addr_t *li)
   return -1;
 }
 
-int pag_malloc_size(size_t size, ptr_t *ptr)
+int pag_malloc_size(size_t size)
 {
   addr_t li;
   if (_pag_find_free_continous_space(size, &li) < 0)
   {
     printf("Continuous space not found\n");
-    return 1;
+    return -1;
   }
 
-  int vpn = pag_get_vpn(li);
+  size_t vpn = pag_get_vpn(li);
 
-  while (vpn <= pag_get_vpn(li + size))
+  while (vpn <= pag_get_vpn(li + size - 1))
   {
-    if (!pag_pages_valid[pag_cur_ipid][vpn])
+    if (!pag_is_page_valid(vpn))
     {
-      pag_pages_valid[pag_cur_ipid][vpn] = 1;
-
       int pfree_page = pag_find_free_page();
       pag_pages_table[pag_cur_ipid][vpn] = pfree_page;
       pag_free_page[pfree_page] = 0;
     }
     vpn++;
   }
-  printf("Found memory va: 0x%zx -> 0x%zx  physical addr: page %d 0x%zx -> 0x%zx\n", li, li + size - 1, pag_pages_table[pag_cur_ipid][pag_get_vpn(li)], pag_get_pa(li), pag_get_pa(li + size - 1));
-  pag_alloc_free_memory(pag_get_pa(li), size, 1);
-
-  ptr->addr = li;
-  pag_update_heap();
-  return 0;
+  
+  pag_alloc_free_memory(li, size, 1);
+  return li;
 }
 
 // END FUNCTIONS
@@ -231,7 +261,6 @@ void m_pag_init(int argc, char **argv)
   pag_pages_count = m_size() / pag_page_size;
 
   pag_pages_table = (int **)malloc(sizeof(int) * MAX_PROC_COUNT);
-  pag_pages_valid = (int **)malloc(sizeof(int) * MAX_PROC_COUNT);
   pag_free_page = (int *)malloc(sizeof(int) * pag_pages_count);
 
   fori(0, MAX_PROC_COUNT)
@@ -241,12 +270,9 @@ void m_pag_init(int argc, char **argv)
     pag_heap[i] = 0;
 
     pag_pages_table[i] = (int *)malloc(sizeof(int) * pag_pages_count);
-    pag_pages_valid[i] = (int *)malloc(sizeof(int) * pag_pages_count);
 
     for (int vpn = 0; vpn < pag_pages_count; vpn++)
-    {
-      pag_pages_valid[i][vpn] = 0;
-    }
+      pag_pages_table[i][vpn] = -1;
   }
 
   fori(0, pag_pages_count) pag_free_page[i] = 1;
@@ -254,25 +280,25 @@ void m_pag_init(int argc, char **argv)
   pag_virtual_memory = (int *)malloc(sizeof(int) * m_size());
 
   fori(0, m_size()) pag_virtual_memory[i] = 0;
-
-  printf(" = = = = = = = = = = = = = = = = = = = =\n");
-  printf("PAG INIT\n");
-  printf("pag_pages_count: %d\n", pag_pages_count);
-  printf("pag_page_size: %ld\n", pag_page_size);
-  printf("pag_offset_bits: %ld\n", pag_offset_bits);
-  printf("pag_free_page_counting: %d\n", pag_free_page_counting());
-  printf(" = = = = = = = = = = = = = = = = = = = =\n");
 }
 
 // Reserva un espacio en el heap de tamaño 'size' y establece un puntero al
 // inicio del espacio reservado.
 int m_pag_malloc(size_t size, ptr_t *out)
 {
-  int ans = pag_malloc_size(size, out);
-  if (ans)
+  pag_update_heap();
+  int ans = pag_malloc_size(size);
+  if (ans < 0)
   {
-    printf("MALLOC HEAP > SIZE:%ld  ANS:%d\n", size, ans);
+    printf("MALLOC HEAP: SIZE:%ld  ANS:%d\n", size, ans);
     return 1;
+  }
+  out->addr = ans;
+  out->size = size;
+
+  if (cero_messanges)
+  {
+    printf("Malloc virtual addr 0x%zx 0x%zx \n", out->addr, ((addr_t)(out->addr + out->size - 1)));
   }
   return 0;
 }
@@ -280,17 +306,20 @@ int m_pag_malloc(size_t size, ptr_t *out)
 // Libera un espacio de memoria dado un puntero.
 int m_pag_free(ptr_t ptr)
 {
-  int vpn = pag_get_vpn(ptr.addr);
+  size_t vpn = pag_get_vpn(ptr.addr);
 
-  while (vpn <= pag_get_vpn(ptr.addr + ptr.size))
+  while (vpn <= pag_get_vpn(ptr.addr + ptr.size - 1))
   {
-    if (!pag_pages_valid[pag_cur_ipid][vpn])
+    if (!pag_is_page_valid(vpn))
+    {
       return 1;
+    }
     vpn++;
   }
 
-  int ex = pag_alloc_free_memory(pag_get_pa(ptr.addr), ptr.size, 0);
+  int ex = pag_alloc_free_memory(ptr.addr, ptr.size, 0);
   pag_updated_valid_pages();
+  pag_update_heap();
   return ex;
 }
 
@@ -300,11 +329,11 @@ int m_pag_push(byte val, ptr_t *out)
   int vpn = pag_get_vpn(pag_stack[pag_cur_ipid]);
   if (vpn < pag_heap[pag_cur_ipid])
   {
-    printf("PUSH ERROR > STACK OVERFLOW: Stack vpn:%d  Heap:%d\n", vpn, pag_heap[pag_cur_ipid]);
+    printf("PUSH ERROR: STACK OVERFLOW: Stack vpn:%d  Heap:%d\n", vpn, pag_heap[pag_cur_ipid]);
     return 1;
   }
 
-  if (!pag_pages_valid[pag_cur_ipid][vpn])
+  if (!pag_is_page_valid(vpn))
   {
     int pp = pag_find_free_page();
 
@@ -314,13 +343,12 @@ int m_pag_push(byte val, ptr_t *out)
       return 1;
     }
 
-    printf("PUSH > Resb a page for Stack vpn:%d\n", vpn);
-    pag_pages_valid[pag_cur_ipid][vpn] = 1;
+    // printf("PUSH > Resb a page for Stack vpn:%d\n", vpn);
     pag_pages_table[pag_cur_ipid][vpn] = pp;
     pag_free_page[pp] = 0;
-    printf("PUSH > Resb a physical page for Stack pa: %d 0x%zx -> 0x%zx\n", pp, pag_get_base_page(pp), pag_get_base_page(pp) + pag_page_size - 1);
-    pag_alloc_free_memory(pag_get_base_page(pp), pag_page_size, 1);
   }
+  
+  pag_alloc_free_memory(pag_stack[pag_cur_ipid], 1, 1);
 
   m_write(pag_get_pa(pag_stack[pag_cur_ipid]), val);
   out->addr = pag_stack[pag_cur_ipid];
@@ -333,21 +361,23 @@ int m_pag_pop(byte *out)
 {
   if (pag_stack[pag_cur_ipid] + 1 > pag_pages_count * pag_page_size)
   {
-    printf("INDEX OUT IN PAG POP > Stack: %ld LS: %ld", pag_stack[pag_cur_ipid] + 1, pag_pages_count * pag_page_size);
+    printf("INDEX OUT IN PAG POP: Stack: %ld ls: %ld", pag_stack[pag_cur_ipid] + 1, pag_pages_count * pag_page_size);
     return 1;
   }
 
   pag_stack[pag_cur_ipid]++;
+
   *out = m_read(pag_get_pa(pag_stack[pag_cur_ipid]));
 
-  int vpn = pag_get_vpn(pag_stack[pag_cur_ipid] - 1);
+  size_t vpn = pag_get_vpn(pag_stack[pag_cur_ipid] - 1);
 
   if (vpn != pag_get_vpn(pag_stack[pag_cur_ipid]))
   {
-    pag_pages_valid[pag_cur_ipid][vpn] = 0;
     pag_free_page[pag_pages_table[pag_cur_ipid][vpn]] = 1;
-    pag_alloc_free_memory(pag_get_base_page(pag_get_vpn(pag_stack[pag_cur_ipid] - 1)), pag_page_size, 0);
+    pag_pages_table[pag_cur_ipid][vpn] = -1;
   }
+
+  pag_alloc_free_memory(pag_stack[pag_cur_ipid], 1, 0);
 
   return 0;
 }
@@ -356,15 +386,15 @@ int m_pag_pop(byte *out)
 int m_pag_load(addr_t addr, byte *out)
 {
 
-  if (!pag_pages_valid[pag_cur_ipid][pag_get_vpn(addr)])
+  if (!pag_is_page_valid(pag_get_vpn(addr)))
   {
-    printf("PAG LOAD > NOT VALID ADDR: 0x%zx VPN: %d VALID: %d\n", addr, pag_get_vpn(addr), pag_pages_valid[pag_cur_ipid][pag_get_vpn(addr)]);
+    printf("PAG LOAD: NOT VALID ADDR: 0x%zx VPN: %ld VALID: %d\n", addr, pag_get_vpn(addr), pag_pages_table[pag_cur_ipid][pag_get_vpn(addr)]);
     return 1;
   }
 
   if (!pag_virtual_memory[pag_get_pa(addr)])
   {
-    printf("PAG LOAD > NOT ALLOC MEMO : addr: 0x%zx  virtual Memo: %d\n", addr, pag_virtual_memory[pag_get_pa(addr)]);
+    printf("PAG LOAD: NOT ALLOC MEMO : addr: 0x%zx  virtual Memo: %d\n", addr, pag_virtual_memory[pag_get_pa(addr)]);
     return 1;
   }
 
@@ -375,21 +405,21 @@ int m_pag_load(addr_t addr, byte *out)
 // Almacena un valor en una dirección determinada
 int m_pag_store(addr_t addr, byte val)
 {
-  if (!pag_pages_valid[pag_cur_ipid][pag_get_vpn(addr)])
+  if (!pag_is_page_valid(pag_get_vpn(addr)))
   {
-    printf("PAG STORE > NOT VALID ADDR: %ld VPN: %d VALID: %d\n", addr, pag_get_vpn(addr), pag_pages_valid[pag_cur_ipid][pag_get_vpn(addr)]);
+    printf("PAG STORE: NOT VALID ADDR: %ld VPN: %ld VALID: %d\n", addr, pag_get_vpn(addr), pag_pages_table[pag_cur_ipid][pag_get_vpn(addr)]);
     return 1;
   }
 
   if (!pag_virtual_memory[pag_get_pa(addr)])
   {
-    printf("PAG STORE > NOT ALLOC MEMO : addr: 0x%zx  virtual Memo: %d\n", addr, pag_virtual_memory[pag_get_pa(addr)]);
+    printf("PAG STORE: NOT ALLOC MEMO : addr: 0x%zx  virtual Memo: %d\n", addr, pag_virtual_memory[pag_get_pa(addr)]);
     return 1;
   }
 
   if (addr > pag_stack[pag_cur_ipid])
   {
-    printf("PAG STORE > Writing in stack: STACK: %ld ADDR: %ld\n", pag_stack[pag_cur_ipid], addr);
+    printf("PAG STORE: Writing in stack: STACK: %ld ADDR: %ld\n", pag_stack[pag_cur_ipid], addr);
     return 1;
   }
 
@@ -400,27 +430,28 @@ int m_pag_store(addr_t addr, byte val)
 // Notifica un cambio de contexto al proceso 'next_pid'
 void m_pag_on_ctx_switch(process_t process)
 {
-  printf("\n = = = = = = = = = = = = = = = = = = = =\n");
-  int index = pag_find_pid(process.pid);
+  // printf("\n = = = = = = = = = = = = = = = = = = = =\n");
+  int temp = pag_cur_ipid;
+  pag_cur_ipid = pag_find_pid(process.pid);
+  set_curr_owner(process.pid);
 
-  if (index < 0)
+  if (pag_cur_ipid < 0)
   {
-    index = pag_free_pid();
+    pag_cur_ipid = pag_free_pid();
 
-    if (index < 0)
+    if (pag_cur_ipid < 0)
     {
       printf("CAMBIO DE CONTEXTO: pid: %d No hay espacios libres\n", process.pid);
+      pag_cur_ipid = temp;
+      set_curr_owner(pag_pids[pag_cur_ipid]);
       return;
     }
-
-    pag_pids[index] = process.pid;
+    pag_pids[pag_cur_ipid] = process.pid;
   }
-
-  pag_cur_ipid = index;
-  printf("CAMBIO DE CONTEXTO > pid:%d, Index: %d Check: %d\n", process.pid, pag_cur_ipid, pag_pids[pag_cur_ipid]);
-  (
-      printf("Stats: Stack: 0%zx  Heap: %d\n", pag_stack[pag_cur_ipid], pag_heap[pag_cur_ipid]));
-  set_curr_owner(process.pid);
+  if (cero_messanges)
+  {
+    printf("Cambio de Contexto %d\n", pag_pids[pag_cur_ipid]);
+  }
 }
 
 // Notifica que un proceso ya terminó su ejecución
@@ -435,35 +466,24 @@ void m_pag_on_end_process(process_t process)
     return;
   }
 
-  int heap_vpn = pag_heap[pag_cur_ipid];
-  printf("heap_vpn: %d\n", heap_vpn);
-  while (heap_vpn >= 0)
+  set_messanges(0);
+  
+  ptr_t aux;
+  for (int vpn = 0; vpn < pag_pages_count; vpn++)
   {
-    if (pag_pages_valid[heap_vpn])
+    if (pag_is_page_valid(vpn))
     {
-      pag_pages_valid[heap_vpn] = 0;
-      pag_free_page[heap_vpn] = 1;
-    }
-    heap_vpn--;
-  }
 
-  printf("pag_cur_ipid: %d\n", pag_cur_ipid);
-  int stack_vpn = pag_get_vpn(pag_stack[pag_cur_ipid]);
-
-  while (stack_vpn < pag_pages_count)
-  {
-    if (pag_pages_valid[stack_vpn])
-    {
-      pag_pages_valid[stack_vpn] = 0;
-      pag_free_page[stack_vpn] = 1;
+      aux.addr = vpn * pag_page_size;
+      aux.size = pag_page_size;
+      m_pag_free(aux);
     }
-    stack_vpn++;
   }
+  set_messanges(1);
 
   // delete procs func
   pag_pids[pag_cur_ipid] = -1;
   pag_stack[pag_cur_ipid] = pag_pages_count * pag_page_size - 1;
   pag_heap[pag_cur_ipid] = 0;
-
   pag_cur_ipid = temp;
 }
