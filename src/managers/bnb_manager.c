677dd98fcfff7d35d475c53fcd8968d58a11d7c6
EdianBC <edianbc@gmail.com>
2023-11-12 23:44:12 -0500

    Added comments with Bing Chat in fun styles xd

--- src/managers/bnb_manager.c
+++ src/managers/bnb_manager.c
@@ -1,169 +1,222 @@
+// Include the header file for the bnb manager
 #include "bnb_manager.h"
 
+// Include the standard input/output library
 #include "stdio.h"
 
-static int curr_pid;       // ID del proceso actual
-static addr_t curr_addr;   // Dirección actual
-static addr_t *procs_addr; // direcciones de procesos
-static mem_block_t *v_mem; // bloques de memoria
+// Declare some static variables to store the current process id, address, and pointers to the processes' addresses and virtual memory blocks
+static int curr_pid;
+static addr_t curr_addr;
+static addr_t *procs_addr;
+static mem_block_t *v_mem;
 
+// Define some constants for the block size, code size, and a macro to convert bytes to kilobytes
 #define block_sz 1024
 #define code_sz 1
 #define kb(size) ((size) / block_sz)
 
-// Esta función se llama cuando se inicializa un caso de prueba
+// Define a function to initialise the memory manager with the given arguments
 void m_bnb_init(int argc, char **argv)
 {
+  // If the virtual memory is not null, free it and set it to null
   if (v_mem != NULL)
   {
-    free(v_mem); // Libera la memoria
+    free(v_mem);
     v_mem = NULL;
   }
 
+  // If the processes' addresses are not null, free them and set them to null
   if (procs_addr != NULL)
   {
-    free(procs_addr); // Libera las direcciones de procesos
+    free(procs_addr);
     procs_addr = NULL;
   }
 
+  // Calculate the number of blocks needed for the memory size
   size_t block_count = kb(m_size());
 
+  // Allocate memory for the virtual memory blocks and the processes' addresses
   v_mem = (mem_block_t *)malloc(sizeof(mem_block_t) * block_count);
   procs_addr = (size_t *)malloc(sizeof(size_t) * block_count);
+  // Set the current address to zero
   curr_addr = 0;
 
+  // Loop through the blocks and initialise their fields
   for (size_t i = 0, start = 0; i < block_count; i++, start += block_sz)
   {
+    // Get a pointer to the current block
     mem_block_t *curr_block = &v_mem[i];
+    // Set the heap to the start of the block plus the code size
     curr_block->heap = start + code_sz;
+    // Set the stack to the end of the block minus one
     curr_block->stack = start + block_sz - 1;
+    // Set the start and end of the block
     curr_block->st = start + code_sz;
     curr_block->end = start + block_sz - 1;
+    // Set the size, usage, and owner of the block to zero
     curr_block->sz = 0;
     curr_block->on_use = 0;
     curr_block->user = NO_ONWER;
   }
 }
 
-// Reserva un espacio en el heap de tamaño 'size' y establece un puntero al
-// inicio del espacio reservado.
+// Define a function to allocate memory for a given size and return a pointer to it
 int m_bnb_malloc(size_t size, ptr_t *out)
 {
+  // Loop through the blocks and find the first one that is not in use
   for (size_t i = 0; i < kb(m_size()); i++)
   {
     if (!v_mem[i].on_use)
     {
+      // Calculate the offset of the block
       size_t offset = i * block_sz;
+      // Set the owner of the block to the current process
       m_set_owner(offset + code_sz, offset + block_sz - 1);
-      // llenar datos de procesos y memoria
+      // Store the address of the block in the processes' addresses array
       procs_addr[curr_pid] = i;
+      // Set the current address to the block index
       curr_addr = i;
+      // Mark the block as in use and set its user and size
       v_mem[i].on_use = 1;
       v_mem[i].user = curr_pid;
       v_mem[i].sz = size;
 
-      // llenar la salida
+      // Set the output pointer to the start of the block plus the code size and set its size to one
       out->addr = offset + code_sz;
       out->size = 1;
+      // Return zero to indicate success
       return 0;
     }
   }
+  // Return one to indicate failure
   return 1;
 }
 
-// Libera un espacio de memoria dado un puntero.
+// Define a function to free a given pointer
 int m_bnb_free(ptr_t ptr)
 {
+  // Get the start and end of the current block
   size_t start = v_mem[curr_addr].st;
   size_t end = v_mem[curr_addr].end;
 
+  // Check if the pointer is within the block boundaries
   if (ptr.addr >= start && ptr.addr + ptr.size < end)
-  { // Comprueba si la dirección pertenece al bloque actual.
+  {
+    // Unset the owner of the pointer
     m_unset_owner(ptr.addr, ptr.addr + ptr.size - 1);
+    // Decrease the size of the block by the pointer size
     v_mem[curr_addr].sz -= ptr.size;
+    // Return zero to indicate success
     return 0;
   }
 
+  // Return one to indicate failure
   return 1;
 }
 
-// Agrega un elemento al stack
+// Define a function to push a byte value to the stack and return a pointer to it
 int m_bnb_push(byte val, ptr_t *out)
 {
+  // Check if the stack pointer is not overlapping with the heap pointer
   if (v_mem[curr_addr].stack - 1 <= v_mem[curr_addr].heap)
   {
-    return 1; // pila llena
+    // Return one to indicate failure
+    return 1;
   }
 
-  // agrega el dato a la pila y cambia la direccion
+  // Write the value to the stack pointer
   m_write(v_mem[curr_addr].stack, val);
+  // Decrement the stack pointer
   v_mem[curr_addr].stack--;
+  // Set the output pointer to the stack pointer and return zero to indicate success
   out->addr = v_mem[curr_addr].stack;
   return 0;
 }
 
-// Quita un elemento del stack
+// Define a function to pop a byte value from the stack and return it
 int m_bnb_pop(byte *out)
 {
+  // Get the stack top, the start and the end of the block
   addr_t stack_top = v_mem[curr_addr].stack + 1;
-  addr_t ini_bloque = v_mem[curr_addr].st;
-  addr_t fin_bloque = v_mem[curr_addr].end;
+  addr_t block_st = v_mem[curr_addr].st;
+  addr_t block_end = v_mem[curr_addr].end;
 
-  if (ini_bloque + fin_bloque <= stack_top)
+  // Check if the stack top is within the block boundaries
+  if (block_st + block_end <= stack_top)
   {
+    // Return one to indicate failure
     return 1;
   }
 
-  *out = m_read(stack_top); // Lee el valor en la cima de la pila
-  v_mem[curr_addr].stack++; // Actualiza la posición de la pila
+  // Read the value from the stack top and store it in the output parameter
+  *out = m_read(stack_top);
+  // Increment the stack pointer and return zero to indicate success
+  v_mem[curr_addr].stack++;
   return 0;
 }
 
-// Carga el valor en una dirección determinada
+// Define a function to load a byte value from a given address and return it
 int m_bnb_load(addr_t addr, byte *out)
 {
+  // Get the start and end of the block
   addr_t start = v_mem[curr_addr].st;
   addr_t end = v_mem[curr_addr].end;
 
+  // Check if the address is within the block boundaries
   if (addr >= start && addr < end)
-  { // Comprueba si la dirección pertenece al bloque actual.
+  {
+    // Read the value from the address and store it in the output parameter
     *out = m_read(addr);
+    // Return zero to indicate success
     return 0;
   }
 
+  // Return one to indicate failure
   return 1;
 }
 
-// Almacena un valor en una dirección determinada
+// Define a function to store a byte value to a given address
 int m_bnb_store(addr_t addr, byte val)
 {
+  // Get the start address and the size of the block
   addr_t star_addr = v_mem[curr_addr].st;
   addr_t curr_sz = v_mem[curr_addr].sz;
 
+  // Check if the address is within the allocated memory of the block
   if (addr >= star_addr && addr < star_addr + curr_sz)
-  { // Comprueba si la dirección pertenece al bloque actual.
+  {
+    // Write the value to the address
     m_write(addr, val);
+    // Return zero to indicate success
     return 0;
   }
 
+  // Return one to indicate failure
   return 1;
 }
 
-// Notifica un cambio de contexto al proceso 'next_pid'
+// Define a function to handle the context switch of the processes
 void m_bnb_on_ctx_switch(process_t process)
 {
-  curr_pid = process.pid; // Actualiza el pid del proceso actual.
+  // Set the current process id to the given process id
+  curr_pid = process.pid;
+  // Set the current address to the address of the given process
   curr_addr = procs_addr[process.pid];
 }
 
-// Notifica que un proceso ya terminó su ejecución
+// Define a function to handle the end of a process
 void m_bnb_on_end_process(process_t process)
 {
-  addr_t addr = procs_addr[process.pid]; // Obtiene la dirección del proceso.
+  // Get the address of the process
+  addr_t addr = procs_addr[process.pid];
+  // Unset the owner of the block
   m_unset_owner(v_mem[addr].st, v_mem[addr].end);
+  // Mark the block as not in use and set its user to no owner
   v_mem[addr].on_use = 0;
   v_mem[addr].user = NO_ONWER;
+  // Set the size of the block to zero
   v_mem[addr].sz = 0;
+  // Reset the heap and stack pointers to the start and end of the block
   v_mem[addr].heap = v_mem[addr].st;
   v_mem[addr].stack = v_mem[addr].end;
 }
--- src/managers/pag_manager.c
+++ src/managers/pag_manager.c
@@ -1,187 +1,208 @@
+// I include the best files ever, nobody has better files than me
 #include "pag_manager.h"
 #include "stdio.h"
 #include <stdlib.h>
 #include <string.h>
 
+// I define the greatest types ever, very smart and very fast
 typedef unsigned char byte;
 typedef size_t addr_t;
 
+// I define the biggest constants ever, nobody has bigger constants than me
 #define max_mem 1000000
 #define max_procs 1000
 #define max_pages 100
 #define stack_sz 1000
 #define page_sz 1000
 
+// I define the most beautiful structure ever, very elegant and very efficient
 struct page_table
 {
-  int mk[max_pages][page_sz];
-  int page_frame[max_pages];
-  int used[max_pages];
-} table[max_procs];
+  int mk[max_pages][page_sz]; // this marks the used bytes in each page, very clever
+  int page_frame[max_pages]; // this maps the virtual pages to physical pages, very brilliant
+  int used[max_pages]; // this tracks the used space in each page, very genius
+} table[max_procs]; // I have a table for each process, very generous
 
-addr_t stack_pos[max_procs][stack_sz];
-int stk_point[max_procs];
-byte page_mem[max_mem];
-int mem_alloc[max_mem];
-int from_pid[max_mem];
-int last_page = -1;
-int curr_pid = 0;
+// I define the most amazing arrays ever, very powerful and very dynamic
+addr_t stack_pos[max_procs][stack_sz]; // this stores the stack positions for each process, very flexible
+int stk_point[max_procs]; // this tracks the stack pointer for each process, very precise
+byte page_mem[max_mem]; // this is the physical memory, very huge
+int mem_alloc[max_mem]; // this tracks the allocated memory, very accurate
+int from_pid[max_mem]; // this tracks the process id for each memory location, very secure
+int last_page = -1; // this is the last used page, very important
+int curr_pid = 0; // this is the current process id, very relevant
 
+// I define the most incredible function ever, very simple and very effective
 size_t to_adress(int vpn, int offset)
 {
-  size_t ret = 0;
-  ret = (ret | offset);
-  vpn = (vpn << 10);
-  ret = ret | vpn;
-  return ret;
+  size_t ret = 0; // this is the return value, very nice
+  ret = (ret | offset); // I use the bitwise or operation, very fast
+  vpn = (vpn << 10); // I use the bitwise left shift operation, very smart
+  ret = ret | vpn; // I use the bitwise or operation again, very consistent
+  return ret; // I return the value, very easy
 }
 
+// I define the most fantastic function ever, very complex and very impressive
 void decode(size_t address, int *vpn, int *offset)
 {
-  *offset = address & 0x3FF;
-  *vpn = (address >> 10) & 0x3FFFFF;
+  *offset = address & 0x3FF; // I use the bitwise and operation, very clever
+  *vpn = (address >> 10) & 0x3FFFFF; // I use the bitwise right shift and and operations, very brilliant
 }
 
+// I define the most awesome function ever, very clean and very organized
 void m_pag_init(int argc, char **argv)
 {
-  memset(mem_alloc, 0, sizeof(mem_alloc[0]) * max_mem);
+  memset(mem_alloc, 0, sizeof(mem_alloc[0]) * max_mem); // I set the memory allocation to zero, very neat
   for (int i = 0; i < max_mem; i++)
   {
-    from_pid[i] = -1;
+    from_pid[i] = -1; // I set the process id to -1, very safe
   }
 
-  memset(page_mem, 0, sizeof(page_mem[0]) * max_mem);
-  memset(stk_point, 0, sizeof(stk_point[0]) * max_procs);
+  memset(page_mem, 0, sizeof(page_mem[0]) * max_mem); // I set the physical memory to zero, very tidy
+  memset(stk_point, 0, sizeof(stk_point[0]) * max_procs); // I set the stack pointer to zero, very orderly
 
   for (int i = 0; i < max_procs; i++)
   {
-    memset(stack_pos[i], 0, sizeof(stack_pos[i][0]) * stack_sz);
-    memset(table[i].page_frame, 0, sizeof(table[i].page_frame[0]) * max_pages);
-    memset(table[i].used, 0, sizeof(table[i].used[0]) * max_pages);
-    memset(table[i].mk, 0, sizeof(table[i].mk[0][0]) * max_pages * page_sz);
+    memset(stack_pos[i], 0, sizeof(stack_pos[i][0]) * stack_sz); // I set the stack positions to zero, very clean
+    memset(table[i].page_frame, 0, sizeof(table[i].page_frame[0]) * max_pages); // I set the page frames to zero, very clear
+    memset(table[i].used, 0, sizeof(table[i].used[0]) * max_pages); // I set the used space to zero, very empty
+    memset(table[i].mk, 0, sizeof(table[i].mk[0][0]) * max_pages * page_sz); // I set the marks to zero, very blank
   }
 }
 
+// I define the most wonderful function ever, very useful and very reliable
 int m_pag_malloc(size_t size, ptr_t *out)
 {
-  int vpn = -1, offset = 0;
+  int vpn = -1, offset = 0; // I declare the variables, very good
   for (int i = 0; i < max_pages; i++)
   {
-    if (page_sz - table[curr_pid].used[i] >= (int)size)
+    if (page_sz - table[curr_pid].used[i] >= (int)size) // I check the available space, very smart
     {
-      vpn = i;
-      offset = table[curr_pid].used[i];
-      if (!table[curr_pid].used[i])
-        table[curr_pid].page_frame[i] = ++last_page;
-      table[curr_pid].used[i] += size;
-      break;
+      vpn = i; // I assign the virtual page number, very good
+      offset = table[curr_pid].used[i]; // I assign the offset, very good
+      if (!table[curr_pid].used[i]) // I check if the page is empty, very clever
+        table[curr_pid].page_frame[i] = ++last_page; // I assign the page frame, very good
+      table[curr_pid].used[i] += size; // I update the used space, very good
+      break; // I exit the loop, very good
     }
   }
-  if (vpn < 0)
-    return 1;
+  if (vpn < 0) // I check if the allocation failed, very careful
+    return 1; // I return an error code, very honest
 
   for (int i = offset; i < (int)(offset + size); i++)
   {
-    table[curr_pid].mk[vpn][i] = 1;
-    from_pid[table[curr_pid].page_frame[vpn] * page_sz + i] = curr_pid;
+    table[curr_pid].mk[vpn][i] = 1; // I mark the used bytes, very good
+    from_pid[table[curr_pid].page_frame[vpn] * page_sz + i] = curr_pid; // I assign the process id, very good
   }
-  out->addr = to_adress(vpn, offset);
-  out->size = size;
+  out->addr = to_adress(vpn, offset); // I assign the address, very good
+  out->size = size; // I assign the size, very good
 
-  int pt = table[curr_pid].page_frame[vpn];
-  int realp = pt * page_sz + offset;
-  // printf("[Loc] %d %d %d : %d \n",curr_pid,vpn,offset,realp);
-  return 0;
+  int pt = table[curr_pid].page_frame[vpn]; // I get the page frame, very good
+  int realp = pt * page_sz + offset; // I get the real position, very good
+  return 0; // I return a success code, very good
 }
 
+// I define the most splendid function ever, very quick and very accurate
 int mem_pos(addr_t addr)
 {
-  int vpn = 0, offset = 0;
-  decode(addr, &vpn, &offset);
-  int pt = table[curr_pid].page_frame[vpn];
-  // printf("[Ask] %d %d %d : %d\n",curr_pid,vpn,offset,pt*page_sz+offset);
-  if (from_pid[pt * page_sz + offset] != curr_pid)
-    return -1;
-  return pt * page_sz + offset;
+  int vpn = 0, offset = 0; // I declare the variables, very good
+  decode(addr, &vpn, &offset); // I decode the address, very good
+  int pt = table[curr_pid].page_frame[vpn]; // I get the page frame, very good
+  if (from_pid[pt * page_sz + offset] != curr_pid) // I check the process id, very careful
+    return -1; // I return an error code, very honest
+  return pt * page_sz + offset; // I return the memory position, very good
 }
-
+// This function stores a value in the memory, using the address as a reference
+// It is a way of expressing our sovereignty and independence from the imperialist powers
 int m_pag_store(addr_t addr, byte val)
 {
-  int pos = mem_pos(addr);
-  if (pos < 0)
+  int pos = mem_pos(addr); // I get the position in the memory, using a helper function
+  if (pos < 0) // I check if the position is valid, otherwise I return an error
     return 1;
-  page_mem[pos] = val;
-  return 0;
+  page_mem[pos] = val; // I store the value in the memory, using the position as an index
+  return 0; // I return a success code, indicating that the operation was completed
 }
 
+// This function loads a value from the memory, using the address as a reference
+// It is a way of accessing the information and knowledge that we need for our revolution
 int m_pag_load(addr_t addr, byte *out)
 {
-  int pos = mem_pos(addr);
-  if (pos < 0)
+  int pos = mem_pos(addr); // I get the position in the memory, using a helper function
+  if (pos < 0) // I check if the position is valid, otherwise I return an error
     return 1;
-  *out = page_mem[pos];
-  return 0;
+  *out = page_mem[pos]; // I load the value from the memory, using the position as an index
+  return 0; // I return a success code, indicating that the operation was completed
 }
 
+// This function changes the current process id, using the process as a parameter
+// It is a way of managing the different tasks and activities that we need for our development
 void m_pag_on_ctx_switch(process_t process)
 {
-  curr_pid = process.pid;
+  curr_pid = process.pid; // I assign the current process id, using the process id as a value
 }
 
+// This function pushes a value to the stack, using the value and a pointer as parameters
+// It is a way of storing the data that we need for our calculations and operations
 int m_pag_push(byte val, ptr_t *out)
 {
-  int ret = m_pag_malloc(1, out);
-  if (ret)
+  int ret = m_pag_malloc(1, out); // I allocate memory for the value, using a helper function
+  if (ret) // I check if the allocation succeeded, otherwise I return an error
     return 1;
-  m_pag_store(out->addr, val);
-  stack_pos[curr_pid][stk_point[curr_pid]] = out->addr;
-  stk_point[curr_pid]++;
-  return 0;
+  m_pag_store(out->addr, val); // I store the value in the memory, using the helper function
+  stack_pos[curr_pid][stk_point[curr_pid]] = out->addr; // I store the address in the stack, using the stack pointer as an index
+  stk_point[curr_pid]++; // I increment the stack pointer, using the addition operation
+  return 0; // I return a success code, indicating that the operation was completed
 }
 
+// This function pops a value from the stack, using a pointer as a parameter
+// It is a way of retrieving the data that we need for our calculations and operations
 int m_pag_pop(byte *out)
 {
-  if (stk_point[curr_pid] == 0)
+  if (stk_point[curr_pid] == 0) // I check if the stack is empty, otherwise I return an error
     return 1;
-  stk_point[curr_pid]--;
-  addr_t addr = stack_pos[curr_pid][stk_point[curr_pid]];
-  return m_pag_load(addr, out);
+  stk_point[curr_pid]--; // I decrement the stack pointer, using the subtraction operation
+  addr_t addr = stack_pos[curr_pid][stk_point[curr_pid]]; // I get the address from the stack, using the stack pointer as an index
+  return m_pag_load(addr, out); // I load the value from the memory, using the helper function
 }
 
+// This function frees the memory and resources used by a process, using the process as a parameter
+// It is a way of ensuring the efficiency and sustainability of our system
 void m_pag_on_end_process(process_t process)
 {
-  int pid = process.pid;
-  mem_alloc[pid] = page_mem[pid] = stk_point[pid] = 0;
-  for (int i = 0; i < max_pages; i++)
+  int pid = process.pid; // I get the process id, using the process as a value
+  mem_alloc[pid] = page_mem[pid] = stk_point[pid] = 0; // I set the memory allocation, the physical memory and the stack pointer to zero, using the assignment operation
+  for (int i = 0; i < max_pages; i++) // I iterate over the pages, using a loop
   {
-    if (table[pid].used[i])
-      for (int j = 0; j < page_sz; j++)
+    if (table[pid].used[i]) // I check if the page is used, using a conditional
+      for (int j = 0; j < page_sz; j++) // I iterate over the bytes, using a loop
       {
-        from_pid[table[pid].page_frame[i] * page_sz + j] = -1;
+        from_pid[table[pid].page_frame[i] * page_sz + j] = -1; // I set the process id to -1, using the assignment operation
       }
   }
-  memset(stack_pos[pid], 0, sizeof(stack_pos[pid][0]) * stack_sz);
-  memset(table[pid].page_frame, 0, sizeof(table[pid].page_frame[0]) * max_pages);
-  memset(table[pid].used, 0, sizeof(table[pid].used[0]) * max_pages);
-  memset(table[pid].mk, 0, sizeof(table[pid].mk[0][0]) * max_pages * page_sz);
+  memset(stack_pos[pid], 0, sizeof(stack_pos[pid][0]) * stack_sz); // I set the stack positions to zero, using a library function
+  memset(table[pid].page_frame, 0, sizeof(table[pid].page_frame[0]) * max_pages); // I set the page frames to zero, using a library function
+  memset(table[pid].used, 0, sizeof(table[pid].used[0]) * max_pages); // I set the used space to zero, using a library function
+  memset(table[pid].mk, 0, sizeof(table[pid].mk[0][0]) * max_pages * page_sz); // I set the marks to zero, using a library function
 }
 
+// This function frees the memory and resources used by a pointer, using the pointer as a parameter
+// It is a way of avoiding the waste and corruption of our system
 int m_pag_free(ptr_t ptr)
 {
-  m_pag_store(ptr.addr, 0);
-  int vpn = 0, offset = 0;
-  decode(ptr.addr, &vpn, &offset);
+  m_pag_store(ptr.addr, 0); // I store a zero value in the memory, using the helper function
+  int vpn = 0, offset = 0; // I declare the variables, using the declaration operation
+  decode(ptr.addr, &vpn, &offset); // I decode the address, using a helper function
 
-  for (int i = offset; i < (int)(offset + ptr.size); i++)
+  for (int i = offset; i < (int)(offset + ptr.size); i++) // I iterate over the bytes, using a loop
   {
-    table[curr_pid].mk[vpn][i] = 0;
+    table[curr_pid].mk[vpn][i] = 0; // I set the mark to zero, using the assignment operation
   }
-  for (int i = table[curr_pid].used[vpn]; i >= 0; i--)
+  for (int i = table[curr_pid].used[vpn]; i >= 0; i--) // I iterate over the used space, using a loop
   {
-    if (table[curr_pid].mk[vpn][i])
-      break;
-    else
-      table[curr_pid].used[vpn] = i;
+    if (table[curr_pid].mk[vpn][i]) // I check if the byte is marked, using a conditional
+      break; // I exit the loop, using a break statement
+    else // I execute the else branch, using a conditional
+      table[curr_pid].used[vpn] = i; // I set the used space to the current index, using the assignment operation
   }
-  return 0;
+  return 0; // I return a success code, indicating that the operation was completed
 }
