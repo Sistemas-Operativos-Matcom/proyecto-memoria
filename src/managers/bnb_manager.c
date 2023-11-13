f634f7c19cfe1ac12c05a11d5520db5e3c3c1efc
Amanda <amandacordero.1709@gmail.com>
2023-11-12 23:13:49 -0500

    terminado

--- src/managers/bnb_manager.c
+++ src/managers/bnb_manager.c
@@ -30,7 +30,9 @@ void m_bnb_init(int argc, char **argv) {
 // Reserva un espacio en el heap de tamaño 'size' y establece un puntero al
 // inicio del espacio reservado.
 int m_bnb_malloc(size_t size, ptr_t *out) {
+  // hay espacio para reservar
   int is = 0;
+  // direccion del espacio a reservar
   int dir = -1;
   for (size_t i = 0; i < 512; i++)
   {
@@ -50,13 +52,13 @@ int m_bnb_malloc(size_t size, ptr_t *out) {
 
     }
     if (is || Memory[proceso].bytes[i] == 3)
+    // llego al stack
     {
       break;
     }
   }
   if (is)
   {
-    
     for (size_t i = dir; i < dir + size; i++)
     {
       Memory[proceso].bytes[i] = 2;
@@ -67,7 +69,6 @@ int m_bnb_malloc(size_t size, ptr_t *out) {
     printf("no cabe mi pana");
     return 1;
   }
-  m_set_owner(Memory[proceso].base + dir, Memory[proceso].base + dir +size);
   out->addr = Memory[proceso].base + dir; 
   out->size = size;
 
@@ -90,7 +91,11 @@ int m_bnb_free(ptr_t ptr) {
 
 // Agrega un elemento al stack
 int m_bnb_push(byte val, ptr_t *out) {
+  // esta lleno el stack
   int full = 0;
+
+  // busca el primer 3 (stack) y añade uno en la posicion anterior si esta 
+  //no es un 2 o 1 (heap o codigo respectivamente)
   for (size_t i = 0; i < 512; i++)
   {
     if (Memory[proceso].bytes[i] == 3)
@@ -100,7 +105,6 @@ int m_bnb_push(byte val, ptr_t *out) {
         Memory[proceso].bytes[i-1] = 3;
         out->addr = Memory[proceso].base +i-1;
         out->size = 1;
-        m_set_owner(Memory[proceso].base +i-1, Memory[proceso].base +i);
         m_write(Memory[proceso].base +i-1,val); 
         return 0;
       }
@@ -114,7 +118,6 @@ int m_bnb_push(byte val, ptr_t *out) {
       Memory[proceso].bytes[511] = 3;
       out->addr = Memory[proceso].base +511;
       out->size = 1;
-      m_set_owner(Memory[proceso].base +511, Memory[proceso].base +511+1);
       m_write(Memory[proceso].base +511, val); 
       return 0;
     }
@@ -125,9 +128,9 @@ int m_bnb_push(byte val, ptr_t *out) {
 
 // Quita un elemento del stack
 int m_bnb_pop(byte *out) {
+  // busca el primer 3 (tope del stack) y devuelve el valor corresp
   for (size_t i = 0; i < 512; i++)
   {
-    // printf("--%d\n", Memory[proceso].bytes[i]);
     if (Memory[proceso].bytes[i] == 3)
     {
       Memory[proceso].bytes[i] = 0;
@@ -140,13 +143,12 @@ int m_bnb_pop(byte *out) {
 
 // Carga el valor en una dirección determinada
 int m_bnb_load(addr_t addr, byte *out) {
-  // printf("--%d",m_read(addr));
   if (addr-(addr % 512) != Memory[proceso].base)
   {
     return 1;
   }
 
-  *out = m_read(addr); // asi se facil?
+  *out = m_read(addr);
   return 0;
 }
 
@@ -168,8 +170,11 @@ int m_bnb_store(addr_t addr, byte val) {
 // Notifica un cambio de contexto al proceso 'next_pid'
 void m_bnb_on_ctx_switch(process_t process) {
   proceso = process.pid;
+  // el proceso ya tiene un fragmento de memoria asignado
   int is = 0;
+  // hay un fragmento de memoria vacio para un nvo proceso
   int st = 0;
+  // direccion del primer fragmento de memoria vacio
   int first = -1;
   for (size_t i = 0; i < m_size()/512; i++)
   {
@@ -186,14 +191,17 @@ void m_bnb_on_ctx_switch(process_t process) {
   }
 
   if (!st && !is)
+  // si no esta y no hay donde ponerlo 
   {
     printf("esto no cabe");
     return;
   }
 
   if (!is)
+  // si no esta se ubica
   {
     Memory[first].process = proceso;
+    m_set_owner(Memory[first].base, Memory[first].base + 511);
     for (size_t i = 0; i < process.program->size; i++)
     {
       Memory[proceso].bytes[i] = 1;
@@ -204,11 +212,13 @@ void m_bnb_on_ctx_switch(process_t process) {
 
 // Notifica que un proceso ya terminó su ejecución
 void m_bnb_on_end_process(process_t process) {
+  // libera el fragemnto de memoria y este puede ser utilizado por otro proceso
   for (size_t i = 0; i < m_size()/512; i++)
   {
     if (Memory[i].process == process.pid)
     {
       Memory[i].process == -1;
+      m_unset_owner(Memory[i].base, Memory[i].base + 511);
       for (size_t j = 0; j < 512; j++)
       {
         Memory[i].bytes[j] = 0;
--- src/managers/pag_manager.c
+++ src/managers/pag_manager.c
@@ -3,77 +3,236 @@
 #include "stdio.h"
 #include "structs.h"
 
-int proceso;
-Page_t *Memory;
+int proc;
+Page_t *Pages;
 Proc_t *Procs;
 
 // Esta función se llama cuando se inicializa un caso de prueba
 void m_pag_init(int argc, char **argv) {
-  //esto hay q arreglarlo
-  Memory = (Page_t *) malloc(m_size()/32*sizeof(Page_t));
+  Pages = (Page_t *) malloc(m_size()/32*sizeof(Page_t));
   Procs = (Proc_t *) malloc(1000*sizeof(Proc_t));
+
+  proc = -1;
+
+  int base = 0;
+
+  for (size_t i = 0; i < m_size()/32; i++)
+  {
+    Pages[i].inUse = 0;
+    Pages[i].base = base;
+    base += 32;
+    for (size_t j = 0; j < 32; j++)
+    {
+      Pages[i].bytes[j] = 0;
+    }
+  }
+
+  for (size_t i = 0; i < 1000; i++)
+  {
+    Procs[i].pid = -1;
+    Procs[i].lpcode = -1;
+    Procs[i].lpstack = -1;
+    Procs[i].lpheap = -1;
+    Procs[i].code = (Page_t *)malloc(100 * sizeof(Page_t ));
+    Procs[i].stack = (Page_t *)malloc(100 * sizeof(Page_t ));
+    Procs[i].heap = (Page_t *)malloc(100 * sizeof(Page_t ));
+  }
+  
+  
 }
 
 // Reserva un espacio en el heap de tamaño 'size' y establece un puntero al
 // inicio del espacio reservado.
 int m_pag_malloc(size_t size, ptr_t *out) {
-  if (Procs[proceso].lpheap == -1)
-  //ehh y si es mas grande que 32???
+  if (size > 32 || Procs[proc].lpheap == -1)
+  // si el heap esta vacio o el espacio a reservar es mayor que el tamaño de una pagina
   {
-    //Buscar una pag vacia y asig space
-    Procs[proceso].lpheap ++;
-    // set owner
-    out->addr = Procs[proceso].code[0].base;
+    // cantidad de paginas vacias consecutivas
+    int len = 0;
+    // indice del inicio de las paginas vacias
+    int count = 0;
+    for (size_t i = 0; i < m_size()/32; i++)
+    {
+      if (!Pages[i].inUse)
+      {
+        len ++;
+        count = i;
+      }
+      else 
+      {
+        len = 0;
+        count = 0;
+      }
+      if (len*32 >= size)
+      {
+        count = count - len +1;
+        break;
+      }
+    }
+    if (len*32 < size)
+    {
+      return 1;
+    }
+    for (size_t i = count; i < count + len; i++)
+    // asigna las paginas al proceso
+    {
+      Procs[proc].lpheap++;
+      Procs[proc].heap[Procs[proc].lpheap] = Pages[i];
+      Pages[i].inUse = 1;
+      m_set_owner(Pages[i].base, Pages[i].base + 31);
+      for (size_t j = 0; j < 32; j++)
+      {
+        Pages[i].bytes[j] = 1;
+      }
+      
+    }
+    Procs[proc].lpheap++;
+    Procs[proc].heap[Procs[proc].lpheap] = Pages[count + len +1];
+    m_set_owner(Pages[count + len +1].base, Pages[count + len +1].base + 31);
+    Pages[count + len +1].inUse = 1;
+    for (size_t j = 0; j < size%32; j++)
+    {
+      Pages[count + len +1].bytes[j] = 1;
+    }
+
+    out->addr = Pages[count].base;
     out->size = size;
     return 0;
   }
 
-  for (size_t i = 0; i < Procs[proceso].lpheap +1; i++)
+  // si size es menor que el tamaño de la pagina busca entre las paginas ya usadas alguna que tenga 
+  // espacio disponible
+
+  int len; 
+  int count;
+  for (size_t i = 0; i < Procs[proc].lpheap +1; i++)
   {
-    //ubic en las pags
+    for (size_t j = 0; j < 32; j++)
+    {
+      if(Procs[proc].heap[i].bytes[j] == 0)
+      {
+        len ++;
+        count = i;
+      }
+      else{
+        len = 0;
+        count = 0;
+      }
+      if (len == size)
+      {
+        count = count - len +1;
+        break;
+      }
+    }
+    if (len != size)
+    {
+      continue;
+    }
+    for (size_t j = count; j < count + len; j++)
+    {
+      Procs[proc].heap[i].bytes[j] = 1;
+    }
+    out->addr = Procs[proc].heap[i].base + count;
+    out->size = size;
+    return 0;
   }
-  
 
+  // si ninguna de las paginas del heap tienen espacio suficiente se usa una nueva pagina
 
+  for (size_t i = 0; i < m_size()/32; i++)
+  {
+    if(!Pages[i].inUse)
+    {
+      Pages[i].inUse = 1;
+      m_set_owner(Pages[i].base,Pages[i].base+31);
+      Procs[proc].lpheap++;
+      Procs[proc].heap[Procs[proc].lpheap] = Pages[i];
+      for (size_t j = 0; j < size; j++)
+      {
+        Procs[proc].heap[i].bytes[j] = 1;
+      }
+      out->addr = Pages[i].base;
+      out->size = size;
+      return 0;
+    }
+  }
+  return 1;
 }
 
 // Libera un espacio de memoria dado un puntero.
 int m_pag_free(ptr_t ptr) {
-  
+  if (ptr.size <= 32)
+  {
+    for (size_t i = 0; i < ptr.size; i++)
+    {
+      Pages[ptr.addr/32].bytes[ptr.addr%32 + i] = 0;
+    }
+    
+    return 0;
+  }
+  int size = ptr.size;
+  for (size_t i = 0; i < size/32; i++)
+  {
+    for (size_t j = 0; j < 32; j++)
+    {
+      Pages[ptr.addr/32 + i].bytes[j] = 0;
+    }
+    
+    size -= 32;
+  }
+  if (size > 0)
+  {
+    for (size_t i = 0; i < size; i++)
+    {
+      Pages[ptr.addr/32 + ptr.size/32].bytes[i] = 0;
+    }
+    
+  }
+  return 0;
 }
 
 // Agrega un elemento al stack
 int m_pag_push(byte val, ptr_t *out) {
-  if (Procs[proceso].lpstack == -1)
+  if (Procs[proc].lpstack == -1)
+  // si no hay ninguna pagina de stack
   {
     for (size_t i = 0; i < m_size()/32; i++)
     {
-      if (!Memory[i].inUse)
+      if (!Pages[i].inUse)
       {
-        Memory[i].inUse = 1;
-        Procs[proceso].lpstack++;
-        Procs[proceso].stack[Procs[proceso].lpstack] = Memory[i];
+        Pages[i].inUse = 1;
+        m_set_owner(Pages[i].base, Pages[i].base + 31);
+        Procs[proc].lpstack++;
+        Procs[proc].stack[Procs[proc].lpstack] = Pages[i];
         break;
       }
     }
   }
   for (size_t i = 0; i < 32; i++)
+  // busca el 1er 0 en la ultima pagina de stack
   {
-    if (Procs[proceso].stack[Procs[proceso].lpstack].bytes[i] == 0)
+    if (Procs[proc].stack[Procs[proc].lpstack].bytes[i] == 0)
     {
-      //escibe y bla bla
+      m_write(Procs[proc].stack[Procs[proc].lpstack].base + i,val);
+      Procs[proc].stack[Procs[proc].lpstack].bytes[i] = 1;
+      out->addr = Procs[proc].stack[Procs[proc].lpstack].base + i;
+      out->size = 1;
       return 0;
     }
   }
   for (size_t i = 0; i < m_size()/32; i++)
+  // si la ultima pagina de stack esta llena se añade una nueva
   {
-    if (!Memory[i].inUse)
+    if (!Pages[i].inUse)
     {
-      Memory[i].inUse = 1;
-      Procs[proceso].lpstack++;
-      Procs[proceso].stack[Procs[proceso].lpstack] = Memory[i];
-      Procs[proceso].stack[Procs[proceso].lpstack].bytes[0] = 1;
-      // escribe y bla bla
+      Pages[i].inUse = 1;
+      m_set_owner(Pages[i].base, Pages[i].base + 31);
+      Procs[proc].lpstack++;
+      Procs[proc].stack[Procs[proc].lpstack] = Pages[i];
+      Procs[proc].stack[Procs[proc].lpstack].bytes[0] = 1;
+      m_write(Procs[proc].stack[Procs[proc].lpstack].base,val);
+      out->addr = Procs[proc].stack[Procs[proc].lpstack].base;
+      out->size = 1;
       return 0;
     }
   }
@@ -82,16 +241,20 @@ int m_pag_push(byte val, ptr_t *out) {
 
 // Quita un elemento del stack
 int m_pag_pop(byte *out) {
-  for (size_t i = 31; i <= 0; i++)
+  // busca el ultimo 1 de la ultima pagina de stack
+  for (size_t i = 31; i >= 0; i--)
   {
-    if (Procs[proceso].stack[Procs[proceso].lpstack].bytes[i])
+    if (Procs[proc].stack[Procs[proc].lpstack].bytes[i])
     {
-      Procs[proceso].stack[Procs[proceso].lpstack].bytes[i] = 0;
+      *out = m_read(Procs[proc].stack[Procs[proc].lpstack].base + i);
+      Procs[proc].stack[Procs[proc].lpstack].bytes[i] = 0;
       if (i == 0)
       {
-        Procs[proceso].lpstack --;
+        // si la pagina se queda vacia pasa a estar disponible
+        Procs[proc].stack[Procs[proc].lpstack].inUse = 0;
+        m_unset_owner(Procs[proc].stack[Procs[proc].lpstack].base, Procs[proc].stack[Procs[proc].lpstack].base + 31);
+        Procs[proc].lpstack --;
       }
-      // leer y to eso
       return 0;
     }
   }
@@ -100,59 +263,133 @@ int m_pag_pop(byte *out) {
 
 // Carga el valor en una dirección determinada
 int m_pag_load(addr_t addr, byte *out) {
-  *out = m_read(addr); // asi se facil?
+  *out = m_read(addr); 
   return 0;
 }
 
 // Almacena un valor en una dirección determinada
 int m_pag_store(addr_t addr, byte val) {
-  m_write(addr,val); // asi se facil?
+  m_write(addr,val); 
   return 0;
 }
 
-// Notifica un cambio de contexto al proceso 'next_pid'
+// Notifica un cambio de contexto al proc 'next_pid'
 void m_pag_on_ctx_switch(process_t process) {
   int is = 0;
   for (size_t i = 0; i < 1000; i++)
   {
     if (process.pid == Procs[i].pid)
     {
-      proceso = i;
+      proc = i;
       is = 1;
       break;
     }
   }
   if (!is)
+  // si el proceso no habia sido ejecutado anteriormente guarda espacio para su codigo
   {
     for (size_t i = 0; i < 1000; i++)
     {
       if(Procs[i].pid == -1)
       {
-        proceso = i;
+        proc = i;
+        Procs[i].pid = process.pid;
         break;
       }
     }
     int code = process.program->size;
+    int len = 0;
+    int count = 0;
     for (size_t i = 0; i < m_size()/32; i++)
     {
-      if (!Memory[i].inUse)
+      if (!Pages[i].inUse)
       {
-        Memory[i].inUse = 1;
-        Procs[proceso].code[Procs[proceso].lpcode] = Memory[i];
-        Procs[proceso].lpcode ++;
-        code -= 32;
+        len ++;
+        count = i;
       }
-      if (code<=0)
+      else 
       {
+        len = 0;
+        count = 0;
+      }
+      if (len*32 >= code)
+      {
+        count = count - len +1;
         break;
       }
     }
+    if (len*32 < code)
+    {
+      return 1;
+    }
+    for (size_t i = count; i < count + len; i++)
+    {
+      Procs[proc].lpcode++;
+      Procs[proc].code[Procs[proc].lpcode] = Pages[i];
+      Pages[i].inUse = 1;
+      for (size_t j = 0; j < 32; j++)
+      {
+        Pages[i].bytes[j] = 1;
+      }
+      
+    }
+    Procs[proc].lpcode++;
+    Procs[proc].code[Procs[proc].lpcode] = Pages[count + len +1];
+    Pages[count + len +1].inUse = 1;
+    for (size_t j = 0; j < code%32; j++)
+    {
+      Pages[count + len +1].bytes[j] = 1;
+    }
+
+    m_set_owner(Pages[count].base, Pages[count].base+code);
+    return 0;
+    
     
   }
 }
 
-// Notifica que un proceso ya terminó su ejecución
+// Notifica que un proc ya terminó su ejecución
 void m_pag_on_end_process(process_t process) {
-  fprintf(stderr, "Not Implemented\n");
-  exit(1);
+  //limpiar las estructuras para que puedan ser utilizadas por otros procesos
+  for (size_t i = 0; i < 1000; i++)
+  {
+    if (process.pid == Procs[i].pid)
+    {
+      Procs[i].pid = -1;
+      for (size_t j = 0; j < Procs[j].lpcode +1; j++)
+      {
+        Procs[i].code[j].inUse = 0;
+        m_unset_owner(Procs[i].code[j].base, Procs[i].code[j].base + 1);
+        for (size_t k = 0; k < 32; k++)
+        {
+          Procs[i].code[j].bytes[k] = 0;
+        }
+      }
+      for (size_t j = 0; j < Procs[j].lpstack +1; j++)
+      {
+        Procs[i].stack[j].inUse = 0;
+        m_unset_owner(Procs[i].stack[j].base, Procs[i].stack[j].base + 1);
+        for (size_t k = 0; k < 32; k++)
+        {
+          Procs[i].stack[j].bytes[k] = 0;
+        }
+      }
+      for (size_t j = 0; j < Procs[j].lpheap +1; j++)
+      {
+        Procs[i].heap[j].inUse = 0;
+        m_unset_owner(Procs[i].heap[j].base, Procs[i].heap[j].base + 1);
+        for (size_t k = 0; k < 32; k++)
+        {
+          Procs[i].heap[j].bytes[k] = 0;
+        }
+      }
+
+      Procs[i].lpcode = -1;
+      Procs[i].lpstack = -1;
+      Procs[i].lpheap = -1;
+
+      return 0;
+    }
+  }
+  return 1;
 }
